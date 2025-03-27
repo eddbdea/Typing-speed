704eb47f6b3fe034cd1121e05720dc9294d618bd
Eduard Badea <eduardbadea@outlook.com>
2025-03-27 23:00:28 +0200

    score implementation, timer and backspace(delete) + final results

--- script.js
+++ script.js
@@ -3,9 +3,80 @@ const primaryWord = document.getElementById('primary-word');
 const secondWord = document.getElementById('secondary-word-1');
 const thirdWord = document.getElementById('secondary-word-2');
 const fourthWord = document.getElementById('secondary-word-3');
-let charIndex = 0;
+const timerElement = document.getElementById('seconds');
+const scoreElement = document.getElementById('score');
+const wordsCounter = document.getElementById('words-counter');
+const startGame = document.getElementById('start-game');
+const resultsContainer = document.getElementById('results');
+const gameResults = document.getElementById('game-result');
+const previousWord = [];
+const stopTimer = 0;
+const backSpace = 'Backspace';
+const space = 'Space';
+const aKey = 'KeyA';
+const zKey = 'KeyZ';
+const oneDigit = 'Digit1';
+const ONE_SECOND_MS = 1000;
+const TEN_SECONDS = 10;
+const ONE_MINUTE_SEC = 60;
+const TURTLE_SPEED = 20;
+const OCTOPUS_SPEED = 40;
+let score = 0;
+let prwIndex = 0;
 let index = 0;
+let charIndex = 0;
+let oneMinuteTimer = 60;
 let finalString = "";
+let validWord = true;
+
+window.addEventListener('keydown', (event) => {
+    if (event.code === oneDigit) {
+        setTimeout(() => {
+            startGame.style.display = 'none';
+            wordsCounter.style.display = 'block';
+            stopTimer = setInterval(timerCountdown, ONE_SECOND_MS);
+        }, ONE_SECOND_MS);
+    }
+}, {once : true});
+
+function timerCountdown() {
+    --oneMinuteTimer;
+    if (oneMinuteTimer === 0) {
+        finalResult(); 
+    } else {
+        updateTimer(oneMinuteTimer);
+    }
+}
+
+function updateTimer(time) {
+    if (time < TEN_SECONDS) {
+        timerElement.innerText = '0' + time;
+    } else { 
+        timerElement.innerText = time;
+    }
+}
+
+function finalResult() {
+    oneMinuteTimer = ONE_MINUTE_SEC;
+    wordsCounter.style.display = 'none';
+    resultsContainer.style.display = 'block';
+    clearInterval(stopTimer);
+    window.removeEventListener('keydown', checkWord, deleteChar, nextWord);
+    showSpeedLevel(score);
+}
+
+function showSpeedLevel(wordsPerMinute) {
+    if (wordsPerMinute <= TURTLE_SPEED) {
+        gameResults.innerText = 'Well..You type with the speed of ' + 
+                                score + 'WPM! You are a TURTLE!';
+    } else if (wordsPerMinute <= OCTOPUS_SPEED) {
+        gameResults.innerText = 'Well..You type with the speed of ' + 
+                                score + 'WPM! You are an OCTOPUS!';
+    } else {
+        gameResults.innerText = 'Well..You type with the speed of ' + 
+                                score + 'WPM! You are a CHEETAH!';
+    }
+}
 
 function updateWords() {
     primaryWord.innerText = `${words[index]}`;
@@ -21,18 +92,36 @@ window.addEventListener('keydown', checkWord);
 function checkWord(event) {
     const currentWord = words[index];
     const wordLength = currentWord.length;
-    changeLetters(event, currentWord, wordLength);
+    if (event.code >= aKey && event.code <= zKey) {
+        changeLetters(event, currentWord, wordLength);
+    }
 }
 
 function changeLetters(event, word, length) {
+    previousWord[prwIndex] = finalString;
     if (event.key === word[charIndex] && charIndex <= length - 1)  {
-        finalString = finalString + `<span class="green">${event.key}</span>`;
+        finalString = finalString + `<span class="correct">${event.key}</span>`;
         updateWord(word);
     } else if (event.key != word[charIndex] && charIndex <= length - 1) {
-        finalString = finalString + `<span class="red">${word[charIndex]}</span>`;
+        finalString = finalString + `<span class="wrong">${word[charIndex]}</span>`;
+        validWord = false;
         updateWord(word);
     } else {
         nextWord();
+    }
+    ++prwIndex;
+}
+
+window.addEventListener('keydown', deleteChar);
+
+function deleteChar(event) {
+    const word = words[index];
+    if (event.code === backSpace && charIndex > 0) {
+        primaryWord.innerHTML = previousWord[prwIndex - 1] + word.slice(charIndex - 1);
+        finalString = previousWord[prwIndex - 1];
+        validWord = true;
+        --prwIndex;
+        --charIndex;
     }
 }
 
@@ -41,9 +130,28 @@ function updateWord(word) {
     ++charIndex;
 }
 
-function nextWord() {
-    charIndex = 0;
-    finalString = "";
-    ++index;
-    updateWords();
+window.addEventListener('keydown', nextWord);
+
+function updateScore(validWord, wordLength) {
+    if (validWord === true && wordLength === charIndex) {
+        ++score;
+        scoreElement.innerText = score;
+    }
+}
+
+function nextWord(event) {
+    const wordLength = words[index].length;
+    if (event.code === space) {
+        updateScore(validWord, wordLength);
+        validWord = true;
+        charIndex = 0;
+        prwIndex = 0;
+        finalString = "";
+        ++index;
+        updateWords();
+    }
+}
+
+function playAgain() {
+    location.reload();
 }
